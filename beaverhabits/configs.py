c09fa32622123c28906f78ca188501c5d39d2958
Rohan <rohan.pudasaini@8squarei.com>
2025-04-03 15:45:53 +0545

    refactor: 🎨Change max user count to 1

--- beaverhabits/configs.py
+++ beaverhabits/configs.py
@@ -38,7 +38,7 @@ class Settings(BaseSettings):
     # Storage
     HABITS_STORAGE: StorageType = StorageType.USER_DATABASE
     DATABASE_URL: str = f"sqlite+aiosqlite:///./{USER_DATA_FOLDER}/habits.db"
-    MAX_USER_COUNT: int = -1
+    MAX_USER_COUNT: int = 1
     JWT_SECRET: str = "SECRET"
     JWT_LIFETIME_SECONDS: int = 60 * 60 * 24 * 30
 
